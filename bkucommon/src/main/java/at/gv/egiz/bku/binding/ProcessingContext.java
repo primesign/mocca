06ae669734a4888432db83599b2bb259a3164021
clemenso <clemenso@8a26b1a7-26f0-462f-b9ef-d0e30c41f5a4>
2008-12-01 12:40:23 +0000

    set/get property
    git-svn-id: https://joinup.ec.europa.eu/svn/mocca/trunk@227 8a26b1a7-26f0-462f-b9ef-d0e30c41f5a4

--- bkucommon/src/main/java/at/gv/egiz/bku/binding/ProcessingContext.java
+++ bkucommon/src/main/java/at/gv/egiz/bku/binding/ProcessingContext.java
@@ -1,6 +1,18 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright 2008 Federal Chancellery Austria and
+ * Graz University of Technology
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package at.gv.egiz.bku.binding;
@@ -8,32 +20,40 @@ package at.gv.egiz.bku.binding;
 import java.util.Hashtable;
 import java.util.Map;
 import java.util.concurrent.Future;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 /**
- * BindingContext?
- * RequestBindingContext?
- * 
- * @author clemens
+ *
+ * @author Clemens Orthacker <clemens.orthacker@iaik.tugraz.at>
  */
 public class ProcessingContext {
 
   public static final String BINDING_PROCESSOR = "binding.processor";
   public static final String FUTURE = "future";
-  
+
+  protected static final Log log = LogFactory.getLog(ProcessingContext.class);
+
   protected Map<String, Object> properties = new Hashtable<String, Object>();
 
   public ProcessingContext(BindingProcessor bp, Future future) {
     properties.put(BINDING_PROCESSOR, bp);
     properties.put(FUTURE, future);
   }
-  
-  
-  
+
   public BindingProcessor getBindingProcessor() {
     return (BindingProcessor) properties.get(BINDING_PROCESSOR);
   }
-  
+
   public Future getFuture() {
-    return (Future) properties.get(FUTURE); 
+    return (Future) properties.get(FUTURE);
+  }
+
+  public Object get(String key) {
+    return properties.get(key);
+  }
+
+  public void put(String key, Object value) {
+    properties.put(key, value);
   }
 }
