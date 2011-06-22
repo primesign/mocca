ea1407ba9756252666e5d67f2397d7cb44ba0232
tkellner <tkellner@8a26b1a7-26f0-462f-b9ef-d0e30c41f5a4>
2011-06-22 14:30:14 +0000

    License change
    copyright headers changed/added
    License text added
    NOTICE modified
    
    
    git-svn-id: https://joinup.ec.europa.eu/svn/mocca/trunk@940 8a26b1a7-26f0-462f-b9ef-d0e30c41f5a4

--- smcc/src/main/java/at/gv/egiz/smcc/PtEidCard.java
+++ smcc/src/main/java/at/gv/egiz/smcc/PtEidCard.java
@@ -1,30 +1,28 @@
 /*
- * Copyright 2010 Federal Chancellery Austria and
- * Graz University of Technology
- * 
+ * Copyright 2011 by Graz University of Technology, Austria
+ * MOCCA has been developed by the E-Government Innovation Center EGIZ, a joint
+ * initiative of the Federal Chancellery Austria and Graz University of Technology.
+ *
  * based on an implementation
- * 
+ *
  * Copyright 2009 Manuel Preliteiro, MULTICERT S.A.
  *
- * Licensed under the EUPL, Version 1.1 or – as soon they 
- * will be approved by the European Commission - subsequent 
- * versions of the EUPL (the "Licence");
- * 
- * You may not use this work except in compliance with the 
- * Licence.
- * 
+ * Licensed under the EUPL, Version 1.1 or – as soon they will be approved by
+ * the European Commission – subsequent versions of the EUPL (the "Licence");
+ * You may not use this work except in compliance with the Licence.
  * You may obtain a copy of the Licence at:
+ * http://www.osor.eu/eupl/
  *
- * http://ec.europa.eu/idabc/eupl
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the Licence is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Licence for the specific language governing permissions and
+ * limitations under the Licence.
  *
- * Unless required by applicable law or agreed to in 
- * writing, software distributed under the Licence is 
- * distributed on an "AS IS" basis,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either 
- * express or implied.
- * 
- * See the Licence for the specific language governing 
- * permissions and limitations under the Licence.
+ * This product combines work with different licenses. See the "NOTICE" text
+ * file for details on the various modules and licenses.
+ * The "NOTICE" text file is part of the distribution. Any derivative works
+ * that you distribute must include a readable copy of the "NOTICE" text file.
  */
 
 package at.gv.egiz.smcc;
