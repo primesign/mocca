1ecf777e78b6d5f32aff4d4f9bff91dbf8060c4a
Christof Rath <christof.rath@prime-sign.com>
2016-11-10 15:05:27 +0100

    Change PrimeSign ATR
    The new ATR is now:
    3b:de:18:00:81:31:fe:58:80:62:c9:03:59:50:72:69:6d:65:53:69:67:6e:d1

--- smcc/src/main/java/at/gv/egiz/smcc/SignatureCardFactory.java
+++ smcc/src/main/java/at/gv/egiz/smcc/SignatureCardFactory.java
@@ -1130,15 +1130,20 @@ public class SignatureCardFactory {
 						(byte) 0xff, (byte) 0xff },
 				"at.gv.egiz.smcc.LIEZertifikatCard"));
 
-		// PrimeSign GmbH ATOS CardOS 5.3
 		supportedCards.add(new SupportedCard(
-				// FL-Post card
-				// [3B:D2:18:00:81:31:FE:58:C9:03:16]
-				new byte[] { (byte) 0x3b, (byte) 0xd2, (byte) 0x18,
+				// PrimeSign GmbH ATOS CardOS 5.3
+				// [3b:de:18:00:81:31:fe:58:80:62:c9:03:59:50:72:69:6d:65:53:69:67:6e:d1]
+				new byte[] { (byte) 0x3b, (byte) 0xde, (byte) 0x18,
 						(byte) 0x00, (byte) 0x81, (byte) 0x31, (byte) 0xFE,
-						(byte) 0x58, (byte) 0xC9, (byte) 0x03, (byte) 0x16 },
+						(byte) 0x58, (byte) 0x80, (byte) 0x62,(byte) 0xC9,
+						(byte) 0x03,  (byte) 0x59, 'P', 'r', 'i', 'm', 'e',
+						'S', 'i', 'g', 'n', (byte) 0xD1},
 				// mask
+				// (ff:ff:ff:ff:ff:ff:ff:ff:ff:ff:ff:ff:ff:ff:ff:ff:ff:ff:ff:ff:ff:ff:ff)
 				new byte[] { (byte) 0xff, (byte) 0xff, (byte) 0xff,
+						(byte) 0xff, (byte) 0xff, (byte) 0xff, (byte) 0xff,
+						(byte) 0xff, (byte) 0xff, (byte) 0xff, (byte) 0xff,
+						(byte) 0xff, (byte) 0xff, (byte) 0xff, (byte) 0xff,
 						(byte) 0xff, (byte) 0xff, (byte) 0xff, (byte) 0xff,
 						(byte) 0xff, (byte) 0xff, (byte) 0xff, (byte) 0xff },
 				"at.gv.egiz.smcc.PrimSignATOSCard"));
