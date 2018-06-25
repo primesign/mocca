9314c6fa22b331fe9addae99af2bdbe183c083f9
Cornelia Lahnsteiner <Cornelia.Lahnsteiner@prime-sign.com>
2018-06-25 11:28:53 +0200

    fix XML validation

--- BKUOnline/src/main/webapp/mocca-js/backend.js
+++ BKUOnline/src/main/webapp/mocca-js/backend.js
@@ -19,100 +19,101 @@ define([], function () {
             enableLogging: true,
             data: sessionId
         });
+    }
 
-        function validateXMLResponse(xml) {
-            if (xml && xml.childNodes) {
-                return validateXMLChildNodes(xml.childNodes, 0);
-            } else {
-                throw 3;
-            }
-        }
-
-        function validateXMLChildNodes(xmlChildNodes, depth) {
-            for (var childNodeIndex in xmlChildNodes) {
-                var childNode = xmlChildNodes[childNodeIndex];
-                if (childNode.nodeName === 'S:Envelope' && depth === 0) {
-                    return validateXMLChildNodes(childNode.childNodes, 1);
-                }
-                else if (childNode.nodeName === 'S:Body') {
-                    return validateXMLChildNodes(childNode.childNodes, 2);
-                }
-                else if (childNode.nodeName === 'GetNextRequestResponse') {
-                    return validateXMLChildNodes(childNode.childNodes, 3);
-                } else if (depth === 3 && (childNode.nodeName === INFOBOX_READ_REQ ||
-                    childNode.nodeName === INFOBOX_SIGN_REQ)) {
-                    return childNode.nodeName;
-                }
-            }
+    function validateXMLResponse(xml) {
+        if (xml && xml.childNodes) {
+            return validateXMLChildNodes(xml.childNodes, 0);
+        } else {
             throw 3;
         }
+    }
 
-        function sendCertificate(sessionId, certificate) {
-            _log.debug('sending certificate to backend');
-
-            var infoboxReadResponse = [
-                '<?xml version="1.0" encoding="UTF-8"?>',
-                '<soapenv:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:stal="http://www.egiz.gv.at/stal">',
-                '<soapenv:Body>',
-                '<stal:GetNextRequest SessionId="',
-                sessionId,
-                '">',
-                '<stal:InfoboxReadResponse>',
-                '<stal:InfoboxValue>',
-                certificate,
-                '</stal:InfoboxValue>',
-                '</stal:InfoboxReadResponse>',
-                '</stal:GetNextRequest>',
-                '</soapenv:Body>',
-                '</soapenv:Envelope>'];
-
-            return $.soap({
-                url: this.baseUrl + '/stal',
-                method: 'nextRequest',
-                namespaceQualifier: 'stal',
-                namespaceURL: 'http://www.egiz.gv.at/stal',
-                appendMethodToURL: false,
-                enableLogging: true,
-                data: infoboxReadResponse.join(''),
-            });
+    function validateXMLChildNodes(xmlChildNodes, depth) {
+        for (var childNodeIndex in xmlChildNodes) {
+            var childNode = xmlChildNodes[childNodeIndex];
+            if (childNode.nodeName === 'S:Envelope' && depth === 0) {
+                return validateXMLChildNodes(childNode.childNodes, 1);
+            }
+            else if (childNode.nodeName === 'S:Body') {
+                return validateXMLChildNodes(childNode.childNodes, 2);
+            }
+            else if (childNode.nodeName === 'GetNextRequestResponse') {
+                return validateXMLChildNodes(childNode.childNodes, 3);
+            } else if (depth === 3 && (childNode.nodeName === INFOBOX_READ_REQ ||
+                childNode.nodeName === INFOBOX_SIGN_REQ)) {
+                return childNode.nodeName;
+            }
         }
+        throw 3;
+    }
 
-        function sendSignedData(sessionId, signedValue) {
-            var signResponse = [
-                '<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:stal="http://www.egiz.gv.at/stal">',
-                '<soapenv:Header/>',
-                '<soapenv:Body>',
-                '<stal:GetNextRequest SessionId="',
-                sessionId,
-                '">',
-                '<stal:SignResponse>',
-                '<stal:SignatureValue>',
-                signedValue,
-                '</stal:SignatureValue>',
-                '</stal:SignResponse>',
-                '</stal:GetNextRequest>',
-                '</soapenv:Body>',
-                '</soapenv:Envelope>'];
+    function sendCertificate(sessionId, certificate) {
+        _log.debug('sending certificate to backend');
 
-            return $.soap({
-                url: this.baseUrl + '/stal',
-                method: 'nextRequest',
-                namespaceQualifier: 'stal',
-                namespaceURL: 'http://www.egiz.gv.at/stal',
-                appendMethodToURL: false,
-                enableLogging: true,
-                data: signResponse.join(''),
-            });
-        }
+        var infoboxReadResponse = [
+            '<?xml version="1.0" encoding="UTF-8"?>',
+            '<soapenv:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:stal="http://www.egiz.gv.at/stal">',
+            '<soapenv:Body>',
+            '<stal:GetNextRequest SessionId="',
+            sessionId,
+            '">',
+            '<stal:InfoboxReadResponse>',
+            '<stal:InfoboxValue>',
+            certificate,
+            '</stal:InfoboxValue>',
+            '</stal:InfoboxReadResponse>',
+            '</stal:GetNextRequest>',
+            '</soapenv:Body>',
+            '</soapenv:Envelope>'];
+
+        return $.soap({
+            url: this.baseUrl + '/stal',
+            method: 'nextRequest',
+            namespaceQualifier: 'stal',
+            namespaceURL: 'http://www.egiz.gv.at/stal',
+            appendMethodToURL: false,
+            enableLogging: true,
+            data: infoboxReadResponse.join(''),
+        });
+    }
+
+    function sendSignedData(sessionId, signedValue) {
+        var signResponse = [
+            '<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:stal="http://www.egiz.gv.at/stal">',
+            '<soapenv:Header/>',
+            '<soapenv:Body>',
+            '<stal:GetNextRequest SessionId="',
+            sessionId,
+            '">',
+            '<stal:SignResponse>',
+            '<stal:SignatureValue>',
+            signedValue,
+            '</stal:SignatureValue>',
+            '</stal:SignResponse>',
+            '</stal:GetNextRequest>',
+            '</soapenv:Body>',
+            '</soapenv:Envelope>'];
+
+        return $.soap({
+            url: this.baseUrl + '/stal',
+            method: 'nextRequest',
+            namespaceQualifier: 'stal',
+            namespaceURL: 'http://www.egiz.gv.at/stal',
+            appendMethodToURL: false,
+            enableLogging: true,
+            data: signResponse.join(''),
+        });
+    }
 
 
-        return {
-            INFOBOX_READ_REQ: INFOBOX_READ_REQ,
-            INFOBOX_SIGN_REQ: INFOBOX_SIGN_REQ,
-            validateXMLResponse: validateXMLResponse,
-            setBaseUrl: setBaseUrl,
-            connect: connect,
-            sendCertificate: sendCertificate,
-            sendSignedData: sendSignedData
-        };
-    });
+    return {
+        INFOBOX_READ_REQ: INFOBOX_READ_REQ,
+        INFOBOX_SIGN_REQ: INFOBOX_SIGN_REQ,
+        validateXMLResponse: validateXMLResponse,
+        setBaseUrl: setBaseUrl,
+        connect: connect,
+        sendCertificate: sendCertificate,
+        sendSignedData: sendSignedData
+    };
+});
