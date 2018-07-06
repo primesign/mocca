758c484ae49999159e31b5faca84f7d14f8986aa
Sandra Kreuzhuber <sandra.kreuzhuber@prime-sign.com>
2018-07-06 14:12:24 +0200

    Add license header with copyright notice

--- BKUOnline/src/main/java/at/gv/egiz/bku/online/filter/CacheControlFilter.java
+++ BKUOnline/src/main/java/at/gv/egiz/bku/online/filter/CacheControlFilter.java
@@ -1,3 +1,24 @@
+/*******************************************************************************
+ * <copyright> Copyright 2018 by PrimeSign GmbH, Graz, Austria </copyright>
+ *
+ * Licensed under the EUPL, Version 1.1 or - as soon they will be approved by
+ * the European Commission - subsequent versions of the EUPL (the "Licence");
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at:
+ * http://www.osor.eu/eupl/
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the Licence is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Licence for the specific language governing permissions and
+ * limitations under the Licence.
+ *
+ * This product combines work with different licenses. See the "NOTICE" text
+ * file for details on the various modules and licenses.
+ * The "NOTICE" text file is part of the distribution. Any derivative works
+ * that you distribute must include a readable copy of the "NOTICE" text file.
+ ******************************************************************************/
+
 package at.gv.egiz.bku.online.filter;
 
 import java.io.IOException;
@@ -14,6 +35,7 @@ public class CacheControlFilter implements Filter {
 
 	@Override
 	public void init(FilterConfig filterConfig) throws ServletException {
+		
 	}
 
 	@Override
@@ -23,10 +45,12 @@ public class CacheControlFilter implements Filter {
 		HttpServletResponse httpResponse = (HttpServletResponse) response;
 		httpResponse.setHeader("Cache-Control", "no-cache");
 		chain.doFilter(request, response);
+		
 	}
 
 	@Override
 	public void destroy() {
+		
 	}
 
 }
